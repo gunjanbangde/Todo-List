0f32a1a84d8a2aec582062aac659572d723412af
Gunjan Bangde <bangdegunjan@gmail.com>
2020-11-03 21:16:38 +0530

    final styling

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,21 +1,25 @@
 //Strik off done items
-$("ul").on("click","li",function(){
-    $(this).toggleClass("striked");    
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("striked");
 });
 
 //Delete the todo
-$("ul").on("click","span",function(event){
-    $(this).parent().fadeOut(500,function(){
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
 });
 
 //Creating new todos
-$("input").on("keypress",function(event){
-    if(event.which === 13){
-        var newTodo =$(this).val();
-        $("ul").append("<li><span>X </span>"+newTodo+"</li>");
+$("input").on("keypress", function (event) {
+    if (event.which === 13) {
+        var newTodo = $(this).val();
+        $("ul").append("<li><span><i class='fas fa-trash'></i> </span>" + newTodo + "</li>");
         $(this).val("");
     }
+});
+
+$(".fa-edit").on("click", function () {
+    $("input").fadeToggle();
 });
